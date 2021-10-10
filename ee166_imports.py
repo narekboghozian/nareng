a11e427ce3611fbfbff07e176c2f2df582126971
narekboghozian <66885970+narekboghozian@users.noreply.github.com>
2021-10-09 18:35:28 -0700

    .

---
+++ ee166_imports.py
@@ -0,0 +1,8 @@
+
+# from .nareng import TL as *
+# from .nareng import EngFormatter as *
+# from .nareng import HomeworkTools as *
+
+# from .nareng.TL import *
+# from .nareng.EngFormatter import *
+# from .nareng.HomeworkTools import *
